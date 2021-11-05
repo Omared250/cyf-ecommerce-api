4665b47cbb768823e39c0cb68da2c2816caf6390
Omar <omared250@gmail.com>
2021-11-05 23:11:42 +0100

    homework completed

--- server.js
+++ server.js
@@ -11,6 +11,29 @@ const connection = new Pool({
     port: '5432'
 })
 
+const getAllCustomers = (req, res) => {
+    connection.query('select * from customers order by id', (error, result) => {
+        res.json(result.rows);
+    })
+}
+
+const getAllSuppliers = (req, res) => {
+    connection.query('select * from suppliers order by id', (error, result) => {
+        res.json(result.rows);
+    })
+}
+
+const getAllproducts = (req, res) => {
+    connection.query('select p.product_name, s.supplier_name from products p inner join suppliers s on s.id=p.supplier_id', 
+    (error, result) => {
+        res.json(result.rows);
+    })
+}
+
+app.get("/customers", getAllCustomers);
+app.get("/suppliers", getAllSuppliers);
+app.get("/products", getAllproducts);
+
 const port = 4000;
 app.listen(port, () => {
     console.log(`app listening on port: ${port}`);
