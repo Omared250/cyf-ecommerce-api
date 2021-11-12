4c2b914db94a5d2fa098e38c6d2de1671d63a738
Omar <omared250@gmail.com>
2021-11-13 00:57:52 +0100

    first exercise homework

---
+++ api.js
@@ -0,0 +1,43 @@
+const { Pool } = require('pg');
+const secret = require('./secret.json');
+const connection = new Pool(secret);
+
+const api = () => {
+    const getAllCustomers = (req, res) => {
+        connection.query('select * from customers order by id', (error, result) => {
+            res.json(result.rows);
+        })
+    }
+
+    const getCustomerById = async (req, res) => {
+        const customerId = req.params.customerId;
+        const query = "select * from customers where id=$1";
+        
+    }
+    
+    const getAllSuppliers = (req, res) => {
+        connection.query('select * from suppliers order by id', (error, result) => {
+            res.json(result.rows);
+        })
+    }
+    
+    const getAllproducts = async (req, res) => {
+        const productName = req.query.productName;
+        const query = "select * from products where product_name like '%$1%'"
+        if (productName) {
+            const result = await connection.query(query, [productName]);
+            return await res.json(result.rows);
+        } else {
+            const allproducts = await connection.query('select * from products order by id');
+            return res.json(allproducts.rows);
+        }
+    }
+    
+    return {
+        getAllCustomers,
+        getAllSuppliers,
+        getAllproducts
+    }
+}
+
+module.exports = api;
--- server.js
+++ server.js
@@ -1,38 +1,13 @@
 const express = require('express');
 const app = express();
 
-const { Pool } = require('pg');
+const apiFunction = require('./api.js');
+const api = apiFunction();
 
-const connection = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'cyf_ecommerce',
-    password: 'Meneer2021',
-    port: '5432'
-})
-
-const getAllCustomers = (req, res) => {
-    connection.query('select * from customers order by id', (error, result) => {
-        res.json(result.rows);
-    })
-}
-
-const getAllSuppliers = (req, res) => {
-    connection.query('select * from suppliers order by id', (error, result) => {
-        res.json(result.rows);
-    })
-}
-
-const getAllproducts = (req, res) => {
-    connection.query('select p.product_name, s.supplier_name from products p inner join suppliers s on s.id=p.supplier_id', 
-    (error, result) => {
-        res.json(result.rows);
-    })
-}
-
-app.get("/customers", getAllCustomers);
-app.get("/suppliers", getAllSuppliers);
-app.get("/products", getAllproducts);
+app.use(express.json());
+app.get("/customers", api.getAllCustomers);
+app.get("/suppliers", api.getAllSuppliers);
+app.get("/products/", api.getAllproducts);
 
 const port = 4000;
 app.listen(port, () => {
